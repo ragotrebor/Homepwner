d5e2416397428dcfc18900b065c8da387ac97309
Roberto García <garciamercado.r@gmail.com>
2017-12-10 17:13:15 -0300

    withdate changer

---
+++ Homepwner/CustomTextField.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTextField.swift
+//  Homepwner
+//
+//  Created by Roberto García on 10-12-17.
+//  Copyright © 2017 Roberto García. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
